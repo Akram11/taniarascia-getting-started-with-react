89937bfb091a758ea4d142a1000638f00af02c4c
Akram <akram.alfawakheeri@bms.bmsoft.de>
2019-04-15 17:45:50 +0200

    created a From class component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
 
   state  = {
       characters : [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
+
     ],
   };
-
 
 
   removeCharacter = index => {
@@ -39,16 +24,12 @@ class App extends Component {
 
 
   render() {
-
-
-
 const { characters } = this.state
 
-
     return (
-
       <div className="container">
        <Table characterData={characters} removeCharacter = {this.removeCharacter} />
+        <Form />
       </div>
     )
   }
